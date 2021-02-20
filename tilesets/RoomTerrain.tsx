265344fbb8d7fcf13aafabb0f39cb1df94e5848d
kasander42 <79368971+kasander42@users.noreply.github.com>
2021-02-20 21:59:21 +0000

    Deploying to gh-pages from master @ 1fc45863733792c6c6dfba7e4ecd3edc6ba7c9a1 🚀

---
+++ tilesets/RoomTerrain.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.02.15" name="RoomTerrain" tilewidth="32" tileheight="32" tilecount="2550" columns="50">
+ <image source="Modern_Interiors/1_Interiors/32x32/Room_Builder_32x32.png" width="1600" height="1632"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="800" wangid="0,0,1,1,0,0,1,0"/>
+   <wangtile tileid="801" wangid="0,0,0,1,1,1,1,0"/>
+   <wangtile tileid="850" wangid="0,1,1,0,0,0,0,0"/>
+   <wangtile tileid="851" wangid="1,1,1,1,1,1,1,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
