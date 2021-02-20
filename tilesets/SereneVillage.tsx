1fc45863733792c6c6dfba7e4ecd3edc6ba7c9a1
KW <kassy@akureyri>
2021-02-20 22:58:47 +0100

    serenevillage tileset

---
+++ tilesets/SereneVillage.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.02.15" name="SereneVillage" tilewidth="32" tileheight="32" tilecount="855" columns="19">
+ <image source="Serene_Village/Serene_Village_32x32.png" width="608" height="1440"/>
+</tileset>
