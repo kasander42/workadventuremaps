3f9676cc2f024fe390cd99df4bd163772f02c55d
Lukas Menzel <lukas@menzel.earth>
2021-02-20 22:35:05 +0100

    add tilesets to tiled

---
+++ tilesets/Interiors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.02.15" name="Interiors" tilewidth="32" tileheight="32" tilecount="8272" columns="16">
+ <image source="Modern_Interiors/1_Interiors/32x32/Interiors_32x32.png" width="512" height="16544"/>
+</tileset>
