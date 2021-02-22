5dc207921d669ef00d3765891ef0196accfdd52a
kasander42 <79368971+kasander42@users.noreply.github.com>
2021-02-22 20:16:28 +0000

    Deploying to gh-pages from master @ bae3a11ec9327a9a038ddfa0aadc5947cba19bf6 🚀

---
+++ tilesets/SuperRetro_Interior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.02.15" name="SuperRetro_Interior" tilewidth="32" tileheight="32" tilecount="768" columns="48">
+ <image source="SuperRetro_Interior/atlas_32x.png" width="1536" height="512"/>
+</tileset>
